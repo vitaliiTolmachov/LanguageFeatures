bb6b9845cbb4627ea379b7d020664009785af06e
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-10 23:10:45 +0200

    Using initializer 4 object creating

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -12,6 +13,23 @@ namespace LanguageFeatures.Controllers
         public string Index()
         {
             return "Navigate to URL to show an example";
+        }
+        public ViewResult AutoPropertyViewResult() {
+            var product = new Product();
+            product.Name = "Kayak";
+            return View("Result", (object) string.Format("Product name: {0}", product.Name));
+        }
+        public ViewResult GetProductCategory()
+        {
+            var product = new Product
+            {
+                Name = "Kayak",
+                ProductID = 100,
+                Category = "Watersports",
+                Description = "A boat for one person",
+                Price = 275m
+            };
+            return View("GetProductCategory", (object)string.Format("Product name: {0}", product.Category));
         }
     }
 }
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        private string _name;
+        public string Name
+        {
+            get { return this.ProductID + _name; }
+            set { _name = value; }
+        }
+
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
