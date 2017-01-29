63543b4e95031a0f3ac9be65b86d8d78e2e4ef0c
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-29 17:12:23 +0200

    Add UnitTest using Moq

---
+++ EssentialToolsUnitTest/UnitTest2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using EssentialTools2.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EssentialToolsUnitTest
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		private Product[] products = new Product[]
+		{
+			new Product {Name = "Kayak", Price = 275m, Category = "Watersports"},
+			new Product {Name = "LifeJacket", Price = 48.49m, Category = "Watersports"},
+			new Product {Name = "Soccer ball", Price = 19.50m, Category = "Soccer"},
+			new Product {Name = "Corner flag", Price = 18, Category = "Soccer"}
+		};
+		[TestMethod]
+		public void SummProductsCorretctly() {
+			//Arrange
+			Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+			mock.Setup(m => m.ApplayDiscount(It.IsAny<decimal>()))
+				.Returns<decimal>(total => total);
+			var target = new LinqValueCalculator(mock.Object);
+
+			//Act
+			decimal summ = target.ValueProducts(products);
+
+			//Assert
+			Assert.AreEqual(products.Sum(p => p.Price), summ);
+		}
+	}
+}
