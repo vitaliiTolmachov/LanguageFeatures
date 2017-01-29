1521f08b3408a6aa6883c967c62f0aa563692d43
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-29 16:20:41 +0200

    Added unit-test Standart

---
+++ EssentialTools2/Models/MinimumDiscountHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EssentialTools2.Models;
+
+namespace EssentialTools2
+{
+	public class MinimumDiscountHelper : IDiscountHelper
+	{
+		public decimal ApplayDiscount(decimal total) {
+
+			if (total < 0) {
+				throw new ArgumentOutOfRangeException();
+			} else if (total > 100) {
+				return Math.Round(total - (total / 100 * 10), 2);
+			} else if (total >= 10 && total <= 100) {
+				return total - 5;
+			} else {
+				return total;
+			}
+		}
+	}
+}
---
+++ EssentialToolsUnitTest/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using EssentialTools2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTools2.Models;
+
+namespace EssentialToolsUnitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		private IDiscountHelper geDiscountHelper() {
+			return new MinimumDiscountHelper();
+		}
+
+		[TestMethod]
+		public void Discount_Above_100() {
+			//Arrange (организация)
+			IDiscountHelper target = this.geDiscountHelper();
+			decimal total = 101m;
+
+			//Act
+			var result = target.ApplayDiscount(total);
+
+			//Asseert
+			Assert.AreEqual(total * 0.9m, result);
+
+
+		}
+		[TestMethod]
+		public void Discount_Between_10_and_100() {
+			//Arrange (организация)
+			IDiscountHelper target = this.geDiscountHelper();
+
+			//Act
+			var tenDollarsDiscount = target.ApplayDiscount(10);
+			var hundredDollarsDiscount = target.ApplayDiscount(100);
+			var fiftyDollarsDiscount = target.ApplayDiscount(50);
+			;
+
+			//Asseert
+			Assert.AreEqual(5, tenDollarsDiscount, "$10 discount si wrong");
+			Assert.AreEqual(95, hundredDollarsDiscount, "$100 discount si wrong");
+			Assert.AreEqual(45, fiftyDollarsDiscount, "$50 discount si wrong");
+			
+
+		}
+		[TestMethod]
+		public void Discount_Between_0_and_10() {
+			//Arrange (организация)
+			IDiscountHelper target = this.geDiscountHelper();
+
+			//Act
+			var fiveDollarsDiscount = target.ApplayDiscount(5);
+			var zeroDollarsDiscount = target.ApplayDiscount(0);
+			
+
+			//Asseert
+			Assert.AreEqual(5, fiveDollarsDiscount);
+			Assert.AreEqual(0, zeroDollarsDiscount);
+
+
+		}
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void Discount_LessThan_0() {
+			//Arrange (организация)
+			IDiscountHelper target = this.geDiscountHelper();
+			decimal total = -1;
+
+			//Act
+			var result = target.ApplayDiscount(-1);
+
+		}
+	}
+}
