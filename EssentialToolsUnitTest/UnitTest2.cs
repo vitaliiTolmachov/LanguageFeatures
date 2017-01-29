81c03a889aa88afc9714d3b7a0e00d5aa22c527f
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-29 18:57:06 +0200

    UnitTests Moq

--- EssentialToolsUnitTest/UnitTest2.cs
+++ EssentialToolsUnitTest/UnitTest2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using EssentialTools2;
 using EssentialTools2.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -16,6 +17,12 @@ namespace EssentialToolsUnitTest
 			new Product {Name = "Soccer ball", Price = 19.50m, Category = "Soccer"},
 			new Product {Name = "Corner flag", Price = 18, Category = "Soccer"}
 		};
+		private Product[] GetProductWithFixedPrice(decimal price) {
+			return new[]
+			{
+				new Product {Price = price}
+			};
+		}
 		[TestMethod]
 		public void SummProductsCorretctly() {
 			//Arrange
@@ -29,6 +36,39 @@ namespace EssentialToolsUnitTest
 
 			//Assert
 			Assert.AreEqual(products.Sum(p => p.Price), summ);
+		}
+		[TestMethod]
+		public void CalculateDiscount() {
+			//Arrange
+			var mock = new Mock<IDiscountHelper>();
+
+			mock.Setup(myMock => myMock.ApplayDiscount(It.IsAny<decimal>()))
+				.Returns<decimal>(total => total);
+			
+			mock.Setup(myMock => myMock.ApplayDiscount(It.Is<decimal>(v => v > 100)))
+							.Returns<decimal>(total => total * 0.9m);
+
+			mock.Setup(myMock => myMock.ApplayDiscount(It.Is<decimal>(v => v < 0)))
+				.Throws<ArgumentOutOfRangeException>();
+
+			mock.Setup(myMock => myMock.ApplayDiscount(It.IsInRange<decimal>(10, 100, Range.Inclusive)))
+				.Returns<decimal>(total => total - 5);
+
+
+			var target = new LinqValueCalculator(mock.Object);
+			//Act
+			decimal discount5 = target.ValueProducts(this.GetProductWithFixedPrice(5));
+			decimal discount10 = target.ValueProducts(this.GetProductWithFixedPrice(10));
+			decimal discount50 = target.ValueProducts(this.GetProductWithFixedPrice(50));
+			decimal discount100 = target.ValueProducts(this.GetProductWithFixedPrice(100));
+			decimal discount500 = target.ValueProducts(this.GetProductWithFixedPrice(500));
+			//Asset
+
+			Assert.AreEqual(5, discount5);
+			Assert.AreEqual(5, discount10);
+			Assert.AreEqual(45, discount50);
+			Assert.AreEqual(95, discount100);
+			Assert.AreEqual(450, discount500);
 		}
 	}
 }
